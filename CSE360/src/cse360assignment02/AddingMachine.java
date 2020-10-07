75d8f7e1e9a3811070687b6718a06fe8ea87134d
Zuoan <hza99@asu.edu>
2020-10-07 01:57:41 -0700

    First commit.

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+/**
+ * This is a class that can doing things
+ * like add or subtract a value to the current total, 
+ * get the current total, print the history that how 
+ * to get that total, and clear the total.
+ * @author Zuoan He
+ *
+ */
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void add (int value) {
+	}
+
+	/**
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+	}
+
+	/**
+	 * 
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * 
+	 */
+	public void clear() {
+	}
+}
